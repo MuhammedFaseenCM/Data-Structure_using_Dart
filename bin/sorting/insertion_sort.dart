f20f236bd39cdf48c687db61d9b959695bf36f98
faseen-hub <faseencm0@gmail.com>
2023-05-08 20:01:57 +0530

    practice

--- bin/sorting/insertion_sort.dart
+++ bin/sorting/insertion_sort.dart
@@ -4,7 +4,6 @@ class InsertionSort {
       int element = array[i];
       int j = i - 1;
       while (j >= 0 && array[j] > element) {
-
         array[j + 1] = array[j];
         j--;
       }
@@ -15,12 +14,25 @@ class InsertionSort {
 
 void main(List<String> args) {
   List<int> array = [41, 30, 22, 14, 10, 7, 5, 2];
-  InsertionSort insertionSort = InsertionSort();
-  insertionSort.sort(array);
+  InsertionSortPractice insertionSort = InsertionSortPractice();
+  insertionSort.insertionSort(array);
   print("");
   print(array);
-
-
 }
 
+class InsertionSortPractice {
+  void insertionSort(List<int> array) {
+    for (var i = 1; i < array.length; i++) {
+      int element = array[i];
+      int j = i - 1;
 
+      while (j >= 0 && array[j] > element) {
+        array[j + 1] = array[j];
+        j--;
+      }
+
+      array[j + 1] = element;
+   
+    }
+  }
+}
